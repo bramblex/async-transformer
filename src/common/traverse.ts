a1248d746be1a6c8bd95e6210552e71dd244bb88
brambles <qjnight@gmail.com>
2022-05-19 01:19:25 +0800

    feat: 准备完成

--- src/common/traverse.ts
+++ src/common/traverse.ts
@@ -1,37 +1,29 @@
+import * as estraverse from 'estraverse';
 
-interface Node {
-  type: string;
+const { VisitorKeys } = estraverse;
+
+interface BaseNode {
+  type: estraverse.NodeType;
+  [key: string]: any;
 }
 
-const isNode = <N extends Node>(target: any): target is N =>
-  target && typeof target.type === 'string';
-
-const isNodeArray = <N extends Node>(target: any): target is N[] =>
-  Array.isArray(target) && target[0] && isNode(target[0]);
-
-const isChildNode = <N extends Node>(target: any): target is N | N[] =>
-  isNodeArray(target) || isNode(target);
-
-const getChildrenKeys = <N extends Node>(node: N): (keyof N)[] =>
-  (Object.keys(node) as (keyof N)[]).filter(key => isChildNode(node[key]));
-
-const traverseChildren = <N extends Node, C>(func: (node: N, ctx: C) => N) => (node: N, ctx: C) => {
-  if (isNode(node)) {
-    for (const key of getChildrenKeys(node)) {
-      const child = node[key] as any as N | N[];
-      if (isNodeArray(child)) {
+function traverseChildren<N extends BaseNode, C>(func: (node: N, ctx: C) => N) {
+  return function (node: N, ctx: C) {
+    for (const key of VisitorKeys[node.type]) {
+      const child = node[key] as BaseNode | BaseNode[];
+      if (Array.isArray(child)) {
         for (let i = 0; i < child.length; i++) {
-          child[i] = child[i] && func(child[i], ctx);
+          child[i] = child[i] && func(child[i] as N, ctx);
         }
       } else {
-        (node as any)[key] = func((node as any)[key], ctx);
+        (node as BaseNode)[key] = func(node[key], ctx);
       }
     }
+    return node;
   }
-  return node;
 }
 
-export const traverse = <N extends Node, C>(func: (node: N, ctx: C, next: (node: N, ctx: C) => N) => N) => {
+export function traverse<N extends BaseNode, C>(func: (node: N, ctx: C, next: (node: N, ctx: C) => N) => N) {
   const _traverse = (node: N, ctx: C): N => func(node, ctx, _traverseChildren);
   const _traverseChildren = traverseChildren(_traverse);
   return _traverse;
